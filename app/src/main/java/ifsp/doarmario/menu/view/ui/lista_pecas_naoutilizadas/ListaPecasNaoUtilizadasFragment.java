b8e0966f0515816129496152ee96ed7c61577e28
YasminDeodato <yasmin.deodato.beatriz@gmail.com>
2019-10-07 17:25:35 -0300

    Revert "Atualização 07-10"
    This reverts commit be2bf1bbf3d6ecbe581613017b7a22bd83b73e81.

---
+++ app/src/main/java/ifsp/doarmario/menu/view/ui/lista_pecas_naoutilizadas/ListaPecasNaoUtilizadasFragment.java
@@ -0,0 +1,39 @@
+package ifsp.doarmario.menu.view.ui.lista_pecas_naoutilizadas;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+import ifsp.doarmario.menu.R;
+import ifsp.doarmario.menu.model.dao.VestuarioDAO;
+import ifsp.doarmario.menu.model.vo.Vestuario;
+import ifsp.doarmario.menu.view.adapter.NUtilizadasAdapter;
+
+public class ListaPecasNaoUtilizadasFragment extends Fragment {
+    private RecyclerView recyclerView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_lista_pecas_nao_utilizadas, container, false);
+        recyclerView = root.findViewById(R.id.recyclerViewN);
+        carregarListaVestuario();
+        return root;
+    }
+
+    public void carregarListaVestuario(){
+        VestuarioDAO vestuarioDAO = new VestuarioDAO( getActivity().getApplicationContext() );
+        List<Vestuario> listaVestuario = vestuarioDAO.listarNaoUtilizadas();
+        NUtilizadasAdapter vestuarioAdapter = new NUtilizadasAdapter( listaVestuario, getActivity().getApplicationContext() );
+
+        //Configurar Recyclerview
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity().getApplicationContext(), 2 );
+        recyclerView.setLayoutManager( layoutManager );
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter( vestuarioAdapter );
+    }
+}
---
+++ app/src/main/java/ifsp/doarmario/menu/view/ui/sobre_mim/SobreMimFragment.java
@@ -0,0 +1,120 @@
+package ifsp.doarmario.menu.view.ui.sobre_mim;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+import ifsp.doarmario.menu.R;
+import ifsp.doarmario.menu.model.dao.UsuarioDao;
+import ifsp.doarmario.menu.model.vo.Usuario;
+
+public class SobreMimFragment extends Fragment {
+    private TextView txtNomeUsuario;
+    private EditText txtEmail;
+    private EditText txtSenha;
+    private ImageButton bttSalvar;
+    private ImageButton bttRemover;
+
+    private String nomeUsuarioAtual;
+    private Usuario usuarioRecuperado;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sobre_mim, container, false);
+
+        //referenciar componentes da tela
+        txtNomeUsuario = view.findViewById(R.id.txtNomeUsuario);
+        txtEmail = view.findViewById(R.id.txtEmail);
+        txtSenha = view.findViewById(R.id.txtSenha);
+        bttSalvar = view.findViewById(R.id.bttSalvar);
+        bttRemover = view.findViewById(R.id.bttRemover);
+
+        //atualizar dados do usuário
+        bttSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //recuperar dados atualizados e setar p/ objeto
+                Usuario usuarioAtualizado = new Usuario();
+                usuarioAtualizado.setNome_usuario(txtNomeUsuario.getText().toString());
+                usuarioAtualizado.setEmail(txtEmail.getText().toString());
+                usuarioAtualizado.setSenha(txtSenha.getText().toString());
+
+                //enviar dados p/ banco
+                boolean atualizar = UsuarioDao.usuarioDao.atualizar(usuarioAtualizado);
+
+                if(atualizar) {
+                    Toast.makeText(getActivity(), R.string.usuario_upd_sucesso, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), R.string.usuario_upd_erro, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //remover conta do usuário
+        bttRemover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //dialog para confirmar exclusão da conta
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+
+                //configurar título e mensagem da dialog
+                dialog.setTitle(R.string.usuario_del_dialog_titulo);
+                dialog.setMessage(R.string.usuario_del_dialog_conteudo);
+
+                //recuperar usuário p/ excluir
+                usuarioRecuperado =  UsuarioDao.usuarioDao.detalhar(nomeUsuarioAtual);
+
+                //configurar botões
+                dialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //enviando dados p/ banco
+                        boolean deletar = UsuarioDao.usuarioDao.deletar(usuarioRecuperado);
+
+                        if(deletar) {
+                            Toast.makeText(getActivity(), R.string.usuario_del_sucesso, Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getActivity(), R.string.usuario_del_erro, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                dialog.setNegativeButton("Não", null );
+
+                //criar e exibir dialog
+                dialog.create();
+                dialog.show();
+            }
+        });
+
+        //recuperar nome do usuário atual
+        nomeUsuarioAtual = (String) getActivity().getIntent().getSerializableExtra("usuario");
+
+        //método p/ recuperar Usuario e exibir dados
+        recuperarUsuario();
+
+        return view;
+    }
+
+    //recuperar dados do usuário para listagem
+    private void recuperarUsuario() {
+        Toast.makeText(getActivity(), nomeUsuarioAtual, Toast.LENGTH_SHORT).show();
+
+        Usuario usuarioRecuperado = UsuarioDao.usuarioDao.detalhar(nomeUsuarioAtual);
+        if(usuarioRecuperado != null) {
+            txtNomeUsuario.setText(usuarioRecuperado.getNome_usuario());
+            txtEmail.setText(usuarioRecuperado.getEmail());
+            txtSenha.setText(usuarioRecuperado.getSenha());
+        } else {
+            Toast.makeText(getActivity(), R.string.erro_listagem, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
